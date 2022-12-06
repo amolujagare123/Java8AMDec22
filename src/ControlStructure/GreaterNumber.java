c885cb97ea59b8b182e95a44e32ef52703ba66a1
amolu <amolujagare@gmail.com>
2022-12-06 09:26:07 +0530

    java

---
+++ src/ControlStructure/GreaterNumber.java
@@ -0,0 +1,31 @@
+package ControlStructure;
+
+public class GreaterNumber {
+
+    /*public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        if(a>b)
+        {
+            System.out.println("a is greater"+a);
+        }
+        else
+        {
+            System.out.println("b is greater");
+        }
+    }*/
+
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int a=100;
+        int b=20;
+        if(a>b) {
+            System.out.println("a is greater"+a);
+        }
+        else {
+            System.out.println("b is greater");
+        }
+    }
+}
---
+++ src/ControlStructure/Marks.java
@@ -0,0 +1,30 @@
+package ControlStructure;
+
+public class Marks {
+
+    public static void main(String[] args) {
+
+        int marks = -89;
+
+
+        if(marks>=0 && marks<=100) {
+
+            if (marks < 40) {
+                System.out.println("Fail");
+                System.out.println("Marks=" + marks);
+            } else if (marks >= 40 && marks < 50)
+                System.out.println("Pass");
+
+            else if (marks >= 50 && marks < 60)
+                System.out.println("Second class");
+
+            else if (marks >= 60 && marks < 75)
+                System.out.println("First class");
+
+            else if (marks >= 75 && marks <= 100)
+                System.out.println("Distinction");
+        }
+        else
+            System.out.println("incorrect input");
+    }
+}
